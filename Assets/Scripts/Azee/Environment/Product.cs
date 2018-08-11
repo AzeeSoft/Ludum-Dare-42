76956d6d3754fc89cb126013c087bd5d2d4fa99c
Azee <aziztitu1996@gmail.com>
2018-08-11 04:48:55 -0500

    Adds ProductManager

---
+++ Assets/Scripts/Azee/Environment/Product.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Product : MonoBehaviour
+{
+    public string ProductName = "";
+
+    // Use this for initialization
+    void Start()
+    {
+        ProductManager.Instance.OnProductAdded(this);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+
+    void OnDestroy()
+    {
+        ProductManager.Instance.OnProductRemoved(this);
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        
+    }
+}
---
+++ Assets/Scripts/Azee/Tools/ProductManager.cs
@@ -0,0 +1,77 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using BasicTools.ButtonInspector;
+using UnityEngine;
+
+public class ProductManager : MonoBehaviour
+{
+    public static ProductManager Instance;
+
+    private Dictionary<string, int> _productCounts = new Dictionary<string, int>();
+
+    [Button("Print Product Counts", "PrintProductCountList")] public bool PrintProductCounts;
+
+    void Awake()
+    {
+        Instance = this;
+    }
+
+	// Use this for initialization
+	void Start () {
+//		CalculateProductCounts();
+//        PrintProductCountList();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void PrintProductCountList()
+    {
+        Debug.Log("Product Counts");
+        Debug.Log("==============");
+        foreach (KeyValuePair<string, int> keyValuePair in _productCounts)
+        {
+            Debug.Log(keyValuePair.Key + ": " + keyValuePair.Value);
+        }
+    }
+
+    public void CalculateProductCounts()
+    {
+        _productCounts.Clear();
+
+        foreach (Product product in FindObjectsOfType<Product>())
+        {
+            OnProductAdded(product);
+        }
+    }
+
+    public int GetProductCount(string productName)
+    {
+        if (_productCounts.ContainsKey(productName))
+        {
+            return _productCounts[productName];
+        }
+
+        return 0;
+    }
+
+    public void OnProductAdded(Product product)
+    {
+        if (!_productCounts.ContainsKey(product.ProductName))
+        {
+            _productCounts[product.ProductName] = 0;
+        }
+
+        _productCounts[product.ProductName]++;
+    }
+
+    public void OnProductRemoved(Product product)
+    {
+        if (_productCounts.ContainsKey(product.ProductName))
+        {
+            _productCounts[product.ProductName]--;
+        }
+    }
+}
