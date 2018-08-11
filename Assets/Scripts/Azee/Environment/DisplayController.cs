1c051630f898a35dd221fe6a326d1a0076a1e0b6
Azee <aziztitu1996@gmail.com>
2018-08-11 09:25:11 -0500

    Implements a simple DisplayController

--- Assets/Scripts/Azee/Environment/DisplayController.cs
+++ Assets/Scripts/Azee/Environment/DisplayController.cs
@@ -1,16 +1,27 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
-public class DisplayController : MonoBehaviour {
+public class DisplayController : MonoBehaviour
+{
+    public Image ProductImage;
+    public TextMeshProUGUI ProductNameLabel;
 
 	// Use this for initialization
 	void Start () {
-		
+		ShowProductOnScreen("Milk");
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		
 	}
+
+    public void ShowProductOnScreen(string productName)
+    {
+//        ProductImage.sprite = productSprite;
+        ProductNameLabel.text = productName;
+    }
 }
