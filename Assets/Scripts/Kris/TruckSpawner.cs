5bcddc91d35c6dc158794de2fe5cea31592c8e40
Kris J <KrisJohnson458@gmail.com>
2018-08-12 16:23:48 -0500

    Added anim stuff for Thomas
    Hopefully it is good but it might not be

--- Assets/Scripts/Kris/TruckSpawner.cs
+++ Assets/Scripts/Kris/TruckSpawner.cs
@@ -12,16 +12,30 @@ public class TruckSpawner : MonoBehaviour {
 
     public int TruckTimer;
 
+    public bool TruckEmpty = false;
+
+    public bool TruckDrive = false;
+
+    void TruckSimulator()
+    {
+        TruckEmpty = true;
+        TruckDrive = true;
+        Debug.Log("We're in the thing");
+        // need animation stuff here
+    }
+
+    void StopSimulation()
+    {
+        TruckEmpty = false;
+        //if you need an exit animation put here
+    }
+
 
     // Use this for initialization
     void Start () {
         StartCoroutine(Shipment());
     }
 	
-	// Update is called once per frame
-	void Update () {
-        
-    }
 
     IEnumerator Shipment()
     {
@@ -35,15 +49,17 @@ public class TruckSpawner : MonoBehaviour {
 
     IEnumerator spawnMoreStuff()
     {
-        for(int i = 0; i < HowMuchStuff; i++)
+        TruckSimulator();
+        for (int i = 0; i < HowMuchStuff; i++)
         {
             List<ProductModel> allProducts = ProductManager.Instance.GetCompleteProductList();
             int random = Random.Range(0, allProducts.Count);
             Instantiate(allProducts[random].ProductPrefab, spawnPoint.position, spawnPoint.rotation);
             yield return new WaitForSeconds(Speed);
         }
+        StopSimulation();
         
     }
-        
+    
     
 }
