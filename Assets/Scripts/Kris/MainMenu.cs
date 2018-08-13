c012ffa15d3b2692d14ed648bd80ce2dfdb5d27b
TomasAbrams <TSAbrams3039@muleriders.saumag.edu>
2018-08-13 13:11:56 -0500

    Adds main menu changes, publication settings, and temp music

--- Assets/Scripts/Kris/MainMenu.cs
+++ Assets/Scripts/Kris/MainMenu.cs
@@ -5,7 +5,61 @@ using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour {
 
-	public void LoadByIndex(int sceneIndex)
+    public GameObject main, htp, cred;
+
+    public enum State
+    {
+        Main,
+        HTP,
+        Credits
+    }
+
+    public State currentState = State.Main;
+
+
+    //tomas change
+    private void Update()
+    {
+        Cursor.visible = true;
+        Cursor.lockState = CursorLockMode.None;
+
+        switch (currentState)
+        {
+            case State.Main:
+                main.gameObject.SetActive(true);
+                htp.gameObject.SetActive(false);
+                cred.gameObject.SetActive(false);
+                break;
+            case State.HTP:
+                main.gameObject.SetActive(false);
+                htp.gameObject.SetActive(true);
+                cred.gameObject.SetActive(false);
+                break;
+            case State.Credits:
+                main.gameObject.SetActive(false);
+                htp.gameObject.SetActive(false);
+                cred.gameObject.SetActive(true);
+                break;
+        }
+
+    }
+
+    public void Back()
+    {
+        currentState = State.Main;
+    }
+
+    public void Credits()
+    {
+        currentState = State.Credits;
+    }
+
+    public void HTP()
+    {
+        currentState = State.HTP;
+    }
+
+    public void LoadByIndex(int sceneIndex)
     {
         SceneManager.LoadScene(sceneIndex);
     }
